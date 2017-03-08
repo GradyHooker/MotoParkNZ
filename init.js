71ca8d47e2a33bc5284988d8c207a03afce3693c
gradyhooker <bloodninja28@gmail.com>
2017-03-09 11:29:51 +1300

    Initial COmmit adding the MotoParkNZ sub-directory from the main gradyhooker.github.io repo into its own project

---
+++ init.js
@@ -0,0 +1,14 @@
+var autocomplete;
+var map;
+var dest_marker;
+var dest_circle;
+var markers = [];
+var directionsService;
+var directionsDisplay;
+var spotsToAdd = [];
+var spotsTrying = 0;
+var spotsAdded = 0;
+var spotsLookedAt = 0;
+var countryRestrict = {
+	'country' : 'nz'
+};
